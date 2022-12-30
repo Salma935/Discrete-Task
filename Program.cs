9c8e1a7d436cd5be8cdd5116eadc5a79bc27b630
Salma935 <121640300+Salma935@users.noreply.github.com>
2022-12-30 19:44:56 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Data;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double n1, n2;
+            Console.WriteLine("represent of the prime Number 5:18");
+            for (n1 = 0; n1 <= 18; n1++)
+            {
+                n2 = 5+2*n1;
+                if (18<=n2)
+                {
+                    continue;
+                }
+                Console.WriteLine(n2);
+            }
+        }
+    }
+} 
---
+++ program 2.cs
@@ -0,0 +1,23 @@
+﻿namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double n1 = 5, n2 = 18;
+            Console.WriteLine("represent of the prefect number");
+            for(n1=0; n1<=n2; n1++)
+            {
+                if(n1<=3 && n1%n1==0 && n1!=1 )
+                {
+                    Console.WriteLine(u(n1));   
+                }
+            }
+
+        }
+        public static double u (double n)
+        {
+            return Math.Pow(n, 2)*(Math.Pow(2,n) -Math.Pow(1,1));
+        }
+    }
+}
